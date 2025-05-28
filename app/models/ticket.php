5b682212daf03c3cb4c733cc5c5ec37a9fb774e8
Kylian2 <kylian.richard@gmail.com>
2025-05-28 23:59:15 +0200

    admin dashboard skeleton

--- app/models/ticket.php
+++ app/models/ticket.php
@@ -13,7 +13,8 @@ class Ticket extends Model {
     public string $user;
 
     public static function getAll() {
-        $request = "SELECT * FROM tickets;";
+        $request = "SELECT id, title, description, status, priority, category, t.created_at, t.updated_at, u.name as user 
+                    FROM tickets t INNER JOIN users u ON u.email = t.user;";
         $result = connexion::pdo()->query($request);
         $result->setFetchmode(PDO::FETCH_CLASS, "Ticket");
         $tickets = $result->fetchAll();
--- app/views/tickets.php
+++ app/views/tickets.php
@@ -1,9 +1,58 @@
-<h1> Tickets </h1>
+<main class="admin-dashboard">
 
-<?php 
+<div class="sidebar">
+    <h1>Admin Dashboard</h1>
+    <div>
+        <div class="sidebar__indicators">
+            <div class="sidebar__indicator sidebar__indicator--new">
+                <span>1</span>
+                <span>Nouveaux</span>
+            </div>
+            <div class="sidebar__indicator sidebar__indicator--in-progress">
+                <span>1</span>
+                <span>En cours</span>
+            </div>
+            <div class="sidebar__indicator sidebar__indicator--closed">
+                <span>1</span>
+                <span>Fermés</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <?php foreach($tickets as $ticket): ?>
+            <div class="ticket-card">
+                <div>
+                    <h5><?php echo $ticket->title; ?></h5>
+                    <span class="material-symbols-rounded">star</span>
+                </div>
+                <p class="ticket-card__description"><?php echo $ticket->description; ?></p>
+                <div class="ticket-card__details">
+                    <?php 
+                        switch ($ticket->priority) {
+                            case 'high':
+                                echo '<span class="ticket-card__priority ticket-card__priority--high">Haute</span>';
+                                break;
+                            case 'medium':
+                                echo '<span class="ticket-card__priority ticket-card__priority--medium">Moyenne</span>';
+                                break;
+                            case 'low':
+                                echo '<span class="ticket-card__priority ticket-card__priority--low">Basse</span>';
+                                break;
+                        }
+                    ?>
+                    <span class="ticket-card__date"><?php echo $ticket->created_at; ?></span>
+                </div>
+                <p class="ticket-card__user"><span class="material-symbols-rounded">person</span><?php echo $ticket->user; ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+<div class="ticket-details">
+    <div class="ticket-details__empty">
+        <span class="material-symbols-rounded">chat_bubble</span>
+        <h2>Selectionnez un ticket</h2>
+        <p>Choisissez un ticket dans la liste pour voir les détails</p>
+    </div>
+</div>
 
-echo "<pre>";
-print_r($tickets);
-echo "</pre>";
-
-?>
+</main>
